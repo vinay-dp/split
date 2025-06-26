223b63d77db51134610af13c765907e8d4262730
vinay-dp <vinayadsmi4@gmail.com>
2025-06-26 18:46:36 +0530

    Create OverlappingAvatars.dart

---
+++ OverlappingAvatars.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:split_share_app/user_avatar.dart';
+
+class OverlappingAvatars extends StatelessWidget {
+  final int count;
+
+  const OverlappingAvatars({super.key, this.count = 2});
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      // width: 20.0 * count,
+      height: 30,
+      child: Stack(
+        children: List.generate(count, (index) {
+          return Positioned(left: index * 20, child: UserAvatar());
+        }),
+      ),
+    );
+  }
+}
